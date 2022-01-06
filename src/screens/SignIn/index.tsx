5c6814f9314cd907999864b2ee348f1a82905eb8
George-b1t <george.soares509@gmail.com>
2022-01-06 15:22:46 -0300

    componentes-inputs-criados

---
+++ src/screens/SignIn/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import { Input } from '@components/Input';
+
+import { Container } from './styles';
+
+export function SignIn() {
+  return (
+    <Container>
+      <Input
+        placeholder='E-mail'
+        type='secondary'
+        autoCorrect={false}
+        autoCapitalize='none'
+      />
+      <Input
+        placeholder='Senha'
+        type='secondary'
+        secureTextEntry
+      />
+    </Container>
+  );
+};

5437f2e3ff47478dd71bdbecad4af01b5c6fbf47
George-b1t <george.soares509@gmail.com>
2022-01-07 19:02:55 -0300

    estilizacao-telaSignin-pronta

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,23 +1,58 @@
 import React from 'react';
+import { KeyboardAvoidingView, Platform } from 'react-native';
 
 import { Input } from '@components/Input';
+import { Button } from '@components/Button';
 
-import { Container } from './styles';
+import brandImg from '@assets/brand.png';
+
+import {
+  Container,
+  Content,
+  Title,
+  Brand,
+  ForgotPassordLabel,
+  ForgotPasswordButton
+} from './styles';
 
 export function SignIn() {
   return (
     <Container>
-      <Input
-        placeholder='E-mail'
-        type='secondary'
-        autoCorrect={false}
-        autoCapitalize='none'
-      />
-      <Input
-        placeholder='Senha'
-        type='secondary'
-        secureTextEntry
-      />
+      <KeyboardAvoidingView
+        behavior={
+        Platform.OS === 'ios'
+        ? 'padding' : undefined
+      }>
+        <Content>
+
+          <Brand source={brandImg} />
+          <Title>Login</Title>
+
+          <Input
+            placeholder='E-mail'
+            type='secondary'
+            autoCorrect={false}
+            autoCapitalize='none'
+          />
+          <Input
+            placeholder='Senha'
+            type='secondary'
+            secureTextEntry
+          />
+
+          <ForgotPasswordButton>
+            <ForgotPassordLabel>
+              Esqueci minha senha
+            </ForgotPassordLabel>
+          </ForgotPasswordButton>
+
+          <Button
+            title='Entrar'
+            type='secondary'
+          />
+
+        </Content>
+      </KeyboardAvoidingView>
     </Container>
   );
 };

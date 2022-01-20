e24d5858f1c0389bbbf9a4a13badc811b2d7bc64
George-b1t <george.soares509@gmail.com>
2022-01-20 11:18:07 -0300

    Criando header do produto

--- src/screens/Product/index.tsx
+++ src/screens/Product/index.tsx
@@ -1,10 +1,18 @@
-import { Container } from './styles';
-import { Platform } from 'react-native';
+import { Container, DeleteLabel, Header, Title } from './styles';
+import { Platform, TouchableOpacity } from 'react-native';
 
 export function Product() {
   return (
     <Container behavior={Platform.OS === 'ios' ? "padding" : undefined}>
 
+      <Header>
+        <Title>Cadastrar</Title>
+        <TouchableOpacity>
+          <DeleteLabel>Deletar</DeleteLabel>
+        </TouchableOpacity>
+
+      </Header>
+      
     </Container>
   );
 };

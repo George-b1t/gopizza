206fbde32e43f8a53d0de4487bf8c34ae946bd35
George-b1t <george.soares509@gmail.com>
2022-01-24 10:35:21 -0300

    Button Back criado

--- src/screens/Product/index.tsx
+++ src/screens/Product/index.tsx
@@ -1,18 +1,20 @@
 import { Container, DeleteLabel, Header, Title } from './styles';
 import { Platform, TouchableOpacity } from 'react-native';
+import { ButtonBack } from '@components/ButtonBack';
 
 export function Product() {
   return (
     <Container behavior={Platform.OS === 'ios' ? "padding" : undefined}>
 
       <Header>
+        <ButtonBack />
         <Title>Cadastrar</Title>
         <TouchableOpacity>
           <DeleteLabel>Deletar</DeleteLabel>
         </TouchableOpacity>
 
       </Header>
-      
+
     </Container>
   );
 };

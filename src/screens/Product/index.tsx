cbc639c82b2e3981a713193de78bfc875da46976
George-b1t <george.soares509@gmail.com>
2022-01-24 11:33:03 -0300

    Utilizando Image Picker

--- src/screens/Product/index.tsx
+++ src/screens/Product/index.tsx
@@ -2,6 +2,8 @@ import { Platform, TouchableOpacity } from 'react-native';
 import { ButtonBack } from '@components/ButtonBack';
 import { Photo } from '@components/Photo';
 
+import * as ImagePicker from 'expo-image-picker';
+
 import {
   Container,
   DeleteLabel,
@@ -10,8 +12,26 @@ import {
   PickImageButton,
   Upload
 } from './styles';
+import { useState } from 'react';
 
 export function Product() {
+  const [ image, setImage ] = useState("");
+
+  async function handlePickerImage() {
+    const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+    if ( status === 'granted' ) {
+      const result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        aspect: [4, 4]
+      });
+
+      if ( !result.cancelled ) {
+        setImage(result.uri);
+      };
+    };
+  };
+
   return (
     <Container behavior={Platform.OS === 'ios' ? "padding" : undefined}>
 
@@ -25,8 +45,12 @@ export function Product() {
       </Header>
 
       <Upload>
-        <Photo uri='https://github.com/George-b1t.png' />
-        <PickImageButton title='Carregar' type='secondary' />
+        <Photo uri={image} />
+        <PickImageButton
+          title='Carregar'
+          type='secondary'
+          onPress={handlePickerImage}
+        />
       </Upload>
 
     </Container>

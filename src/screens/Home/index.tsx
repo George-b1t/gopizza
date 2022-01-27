04742a8a3ceb50e884bd94f70264666115d01557
George-b1t <george.soares509@gmail.com>
2022-01-27 16:44:10 -0300

    Header da Home Screen Criado

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,11 +1,33 @@
 import React from 'react';
+import { MaterialIcons } from '@expo/vector-icons';
+import { useTheme } from 'styled-components';
+import { TouchableOpacity } from 'react-native';
 
-import { Container } from './styles';
+import happyEmoji from '@assets/happy.png';
+
+import {
+  Container,
+  Header,
+  Greeting,
+  GreetingEmoji,
+  GreetingText
+} from './styles';
 
 export function Home() {
+  const { COLORS } = useTheme();
+
   return (
     <Container>
+      <Header>
+        <Greeting>
+          <GreetingEmoji source={happyEmoji} />
+          <GreetingText>Olá, Admin</GreetingText>
+        </Greeting>
 
+        <TouchableOpacity>
+          <MaterialIcons name='logout' color={COLORS.TITLE} size={24} />
+        </TouchableOpacity>
+      </Header>
     </Container>
   );
 };

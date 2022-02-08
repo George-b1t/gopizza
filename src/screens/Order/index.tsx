854a20555dd4c718beafcfba3cf33ab90cc09f10
George-b1t <george.soares509@gmail.com>
2022-02-08 14:34:03 -0300

    Formulario de pedido

--- src/screens/Order/index.tsx
+++ src/screens/Order/index.tsx
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
-import { Platform } from 'react-native';
+import { Platform, ScrollView } from 'react-native';
 
 import { ButtonBack } from '@components/ButtonBack';
 import { RadioButton } from '@components/RadioButton';
+import { Input } from '@components/Input';
+import { Button } from '@components/Button';
 
 import { PIZZA_TYPES } from '@utils/pizzaTypes';
 
@@ -10,7 +12,14 @@ import {
   Container,
   Header,
   Photo,
-  Sizes
+  Sizes,
+  Form,
+  FormRow,
+  InputGroup,
+  Label,
+  Price,
+  Title,
+  ContentScroll
 } from './styles';
 
 export function Order() {
@@ -18,25 +27,50 @@ export function Order() {
 
   return (
     <Container behavior={Platform.OS === 'ios' ? 'padding' : undefined}>
-      <Header>
-        <ButtonBack
-          onPress={() => {}}
-          style={{ marginBottom: 108 }}
-        />
-      </Header>
-      <Photo source={{ uri: 'https://github.com/George-b1t.png' }} />
-      <Sizes>
-        {
-          PIZZA_TYPES.map(item => (
-            <RadioButton
-              key={item.id}
-              title={item.name}
-              selected={size === item.id}
-              onPress={() => setSize(item.id)}
-            />
-          ))
-        }
-      </Sizes>
+      <ContentScroll>
+        <Header>
+          <ButtonBack
+            onPress={() => {}}
+            style={{ marginBottom: 108 }}
+          />
+        </Header>
+
+        <Photo source={{ uri: 'https://github.com/George-b1t.png' }} />
+
+        <Form>
+          <Title>Nome da Pizza</Title>
+          <Label>Selecione um tamanho</Label>
+
+          <Sizes>
+            {
+              PIZZA_TYPES.map(item => (
+                <RadioButton
+                  key={item.id}
+                  title={item.name}
+                  selected={size === item.id}
+                  onPress={() => setSize(item.id)}
+                />
+              ))
+            }
+          </Sizes>
+
+          <FormRow>
+            <InputGroup>
+              <Label>Número da mesa</Label>
+              <Input keyboardType='numeric' />
+            </InputGroup>
+
+            <InputGroup>
+              <Label>Quantidade</Label>
+              <Input keyboardType='numeric' />
+            </InputGroup>
+          </FormRow>
+
+          <Price>Valor de R$ 00,00</Price>
+
+          <Button title='Confirmar pedido' />
+        </Form>
+      </ContentScroll>
     </Container>
   );
 };

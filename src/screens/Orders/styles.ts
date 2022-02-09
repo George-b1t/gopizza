2594e967844e6ebc1523e74a0c0fcb64a0b22f16
George-b1t <george.soares509@gmail.com>
2022-02-09 16:13:47 -0300

    Header de pedidos

--- src/screens/Orders/styles.ts
+++ src/screens/Orders/styles.ts
@@ -1,6 +1,26 @@
-import styled from "styled-components/native";
+import { LinearGradient } from "expo-linear-gradient";
+import { getStatusBarHeight } from "react-native-iphone-x-helper";
+import styled, { css } from "styled-components/native";
 
 export const Container = styled.View`
   flex: 1;
   background-color: ${({ theme }) => theme.COLORS.BACKGROUND};
+`;
+
+export const Title = styled.Text`
+  font-size: 24px;
+  text-align: center;
+
+  ${({ theme }) => css`
+    font-family: ${theme.FONTS.TITLE};
+    color: ${theme.COLORS.TITLE};
+  `};
+`;
+
+export const Header = styled(LinearGradient).attrs(({
+  theme
+}) => ({
+  colors: theme.COLORS.GRADIENT
+}))`
+  padding: ${getStatusBarHeight() + 33}px 0 33px;
 `;

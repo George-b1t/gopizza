ff4716238d5c8a9f0da01670ecc05d07a3442e57
George-b1t <george.soares509@gmail.com>
2022-02-16 10:24:10 -0300

    Finalizado componente de pedido

--- src/screens/Orders/index.tsx
+++ src/screens/Orders/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
+import { FlatList } from 'react-native-gesture-handler';
 
 import { OrderCard } from '@components/OrderCard';
-
 import { Container, Header, Title } from './styles';
-import { FlatList } from 'react-native-gesture-handler';
+import { ItemSeparator } from '@components/ItemSeparator';
 
 export function Orders() {
   return (
@@ -16,11 +16,12 @@ export function Orders() {
         data={['1', '2', '3']}
         keyExtractor={item => item}
         renderItem={({ item, index }) => (
-          <OrderCard  index={index} />
+          <OrderCard index={index} />
         )}
         numColumns={2}
         showsVerticalScrollIndicator={false}
-        contentContainerStyle={{ paddingBottom: 20 }}
+        contentContainerStyle={{ paddingBottom: 24, paddingHorizontal: 24 }}
+        ItemSeparatorComponent={ItemSeparator}
       />
     </Container>
   );
